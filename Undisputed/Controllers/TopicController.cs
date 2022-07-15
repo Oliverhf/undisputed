45c84d42fcbfc04237d7e0634173dcb674aaad30
unknown <oliver.hfavalli@gmail.com>
2022-07-14 23:16:16 -0300

    feat: CreateTopicViewModel, Topic Create, CSS site

--- Undisputed/Controllers/TopicController.cs
+++ Undisputed/Controllers/TopicController.cs
@@ -3,17 +3,22 @@ using Microsoft.EntityFrameworkCore;
 using Undisputed.Data;
 using Undisputed.Interfaces;
 using Undisputed.Models;
+using Undisputed.ViewModels;
 
 namespace Undisputed.Controllers
 {
+    
     public class TopicController : Controller
     {
         private readonly ITopicRepository _topicRepository;
+
 
         public TopicController(ITopicRepository topicRepository)
         {
             _topicRepository = topicRepository;
         }
+
+  
         public async Task<IActionResult> Index()
         {
             IEnumerable<Topic> topics = await _topicRepository.GetAll();
@@ -25,6 +30,85 @@ namespace Undisputed.Controllers
             Topic topic = await _topicRepository.GetByIdAsync(id);
             return View(topic);
         }
+
+  
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTopicViewModel topicVM)
+        {
+            if(ModelState.IsValid)
+            {
+                var topic = new Topic
+                {
+                    Title = topicVM.Title,
+                    Description = topicVM.Description,
+                    Image = topicVM.Image.ToString(),
+                    TopicCategory = topicVM.TopicCategory,
+                    AppUserId = topicVM.AppUserId,
+                    Address = new Address
+                    {
+                        Street = topicVM.Address.Street,
+                        City = topicVM.Address.City,
+                        State = topicVM.Address.State,
+                    }
+                };
+                _topicRepository.Add(topic);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Topic creation failed");
+            }
+            
+            return View(topicVM);
+            
+        }
+
+
+
+        [Route("api/[Controller]")]
+        //[ApiController]
+        [Produces("application/json")]
+        [HttpGet]
+        public async Task<IEnumerable<Topic>> Get()
+        {
+
+            try
+            {
+                return await _topicRepository.GetAll();
+
+            }
+            catch (Exception ex)
+            {
+                return (IEnumerable<Topic>)BadRequest("Failed to get topics");
+            }
+
+        }
+
+        [HttpGet("api/[Controller]/{id:int}")]
+        public async Task<Topic> Get(int id)
+        {
+            try
+            {
+               var topic = await _topicRepository.GetByIdAsync(id);
+
+                if (topic != null) return topic;
+                //NotFound();
+                else return null;
+                
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
     }
 
 }
---
+++ Undisputed/ViewModels/CreateTopicViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Undisputed.Data.Enum;
+using Undisputed.Models;
+
+namespace Undisputed.ViewModels
+{
+    public class CreateTopicViewModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public Address? Address { get; set; }
+        public TopicCategory TopicCategory { get; set; }
+        public string? AppUserId { get; set; }
+        public AppUser? AppUser { get; set; }
+        public DateTime? DateStarted { get; set; }
+    }
+}
