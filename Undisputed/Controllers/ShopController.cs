e58d65c540f7f0a5f1eb7ebe15fe485d01abd45d
unknown <oliver.hfavalli@gmail.com>
2022-07-21 01:49:35 -0300

    feat: wwwroot/client, app/views, app/services, app/component html, client/angular json (output path to wwwroot), _Layout changes for head section inject and app component, app module, shop index using angular UI

--- Undisputed/Controllers/ShopController.cs
+++ Undisputed/Controllers/ShopController.cs
@@ -1,17 +1,43 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Undisputed.Models;
 
 namespace Undisputed.Controllers
 {
     public class ShopController : Controller
     {
+
+        List<string> listOfProducts = new List<string>()
+        {
+            "T-shirt",
+            "Mugs",
+            "Caps",
+        };
+
         [Authorize]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        [Route("api/[Controller]/products")]
+
+        public List<string> Products()
+        {
+
+            try
+            {
+                return listOfProducts;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+          
+        }
 
     }
 }
--- Undisputed/client/src/app/app.module.ts
+++ Undisputed/client/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { Store } from './services/store.service';
+import ProductListView from './views/productListView.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+        AppComponent,
+        ProductListView
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpClientModule
   ],
-  providers: [],
+  providers: [
+      Store
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
